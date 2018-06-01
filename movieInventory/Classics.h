1f537b326bc492ee400bdf6fd436655ed0c2408a
Chhoden Gurung <chhoden.grg@hotmail.com>
2018-05-31 21:58:38 -0700

    Added Movie, Comedy, Drama and Classics

--- movieInventory/Classics.h
+++ movieInventory/Classics.h
@@ -1,24 +1,32 @@
 //
 // Created by Goitom Hadishe on 5/15/18.
 //
+#include<string>
 #include "Movie.h"
+using namespace std;
 #ifndef CSS343ASS_4_CLASSICS_H
 #define CSS343ASS_4_CLASSICS_H
 
 
-class Classic: public Movie {
-public:
-    Classic(int stock, string title, string Director,
-                 string majorFirst, string majorLast, int releaseMonth, int releaseYear);		//default constructor
-    ~Classic();	//destructor
-    //returns information about the classic as formatted output.
-    // this includes stock counts
-    int getReleaseMonth();
-    string getMajorChar();
-
+class Classic :public Movie {
 private:
 	int realseMonth;
-	string majorChar;
+	string majorFirsNa;
+	string majorLastNa;
+public:
+	Classic(int stock, string title, string Director,
+		string majorFirsNa, string majorLastNa, int releaseMonth, int year);		//default constructor
+	~Classic();	//destructor
+				//returns information about the classic as formatted output.
+				// this includes stock counts
+	int getReleaseMonth();
+	string getgmajorFirsNa();
+	string getmajorLastNa();
+	virtual void print();
+	virtual bool operator>(const Movie &m) const;
+	virtual bool operator>=(const Movie &m) const;
+	virtual bool operator==(const Movie &m) const;
+	virtual bool operator!=(const Movie& m)const;
 };
 
 
