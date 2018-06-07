131b28a9c3e26c6c506e511d49e6f7bb1ef6d99f
GOITOM K HADISHE <goitomhadishe@yahoo.com>
2018-06-06 23:17:08 -0700

    Update Drama.h

--- movieInventory/Drama.h
+++ movieInventory/Drama.h
@@ -11,13 +11,21 @@ using namespace std;
 
 class Drama :public Movie {
 public:
+	/** constructor which take stock, itle, irector, year, and mtn as parameter **/
 	Drama(int stock, string title, string director, int year, int mtn);
+	 /**distructor to clear up the new drama **/
 	~Drama();
-
+        /**print all the drama movies **/
 	virtual void print();
+	 /** oerator to compare if one drama movie is greater than the other dram movie
+          *  **/
 	virtual bool operator>(Drama &m);
+	/** oerator to compare if one drama movie is less than the other dram movie
+         *  **/
 	virtual bool operator<(Drama &m);
+	/**oerator to compare if two different Drama movies are equal  **/
 	virtual bool operator==(Drama &m);
+	/**boolean oerator to compare if two different Drama movies are not equal **/
 	virtual bool operator!=(Drama &m);
 };
 
