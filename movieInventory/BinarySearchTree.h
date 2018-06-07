686ae79ef49774e47e9d5824b884f92c43eb7898
GOITOM K HADISHE <goitomhadishe@yahoo.com>
2018-06-06 23:29:26 -0700

    Update BinarySearchTree.h

--- movieInventory/BinarySearchTree.h
+++ movieInventory/BinarySearchTree.h
@@ -9,15 +9,21 @@
 
 class BinarySearchTree {
 public:
+	 /** default constructor **/
 	BinarySearchTree();
+	/** destructor to free up un nessary node  **/
 	virtual ~BinarySearchTree();
-
+	/** add the movie to the binnary serach tree  **/
 	bool add(Movie *m);
+	   /** this will check if the the movie is in the tree,
+        * if so then it will return true, if its not then it will return false**/
 	bool contains(Movie *m);
 	Movie* search(string key);
+	/** print all the node tp the output  **/
 	void clear();
+	/** clear all the node from the tree **/
 	void print();
-
+        /** root pointer **/
 	BinaryNode *root;
 
 private:
