1fa04e740902c7461f47659c850623b9c551a279
GOITOM K HADISHE <goitomhadishe@yahoo.com>
2018-06-06 23:20:28 -0700

    Update Customer.h

--- movieInventory/Customer.h
+++ movieInventory/Customer.h
@@ -11,26 +11,33 @@ using namespace std;
 class Customer
 {
 public:
-	//Default constructor
+	 /**Default constructor **/
 	Customer();
 
-	//Constructor
+	/**Constructor**/
 	Customer(string id, string fn, string ln, int ctn);
 
-	//Destructor
+	/**Destructor**/
 	~Customer();
 
 	//Accessors
+	/** get the customer id  **/
 	string getCustomerID()const;
+	/** get the first name  **/
 	string getFirstName()const;
+	/** get the last  name  **/
 	string getLastName()const;
+	/** get the customer table   **/
 	int getCustomerTableNum()const;
+	/** this will get the list of borrowed movie   **/
 	vector<string> getBorrowedMovies() const;
-
+        /** get transaction  **/
 	TransactionList getTransactions()const;
-
+        /** this will return true if the movie title is added to the ransaction, and will return false    **/
 	bool addTransaction(char actionType, string movieTitle);
+	/** this will return true if the movie title is added to the ransaction   **/
 	bool borrowMovie(string movieID);
+	 /** this will return movie    **/
 	bool returnMovie(string movieID);
 
 private:
