bd9b14453308e6015122958b10537e670cc371f0
GOITOM K HADISHE <goitomhadishe@yahoo.com>
2018-06-06 23:30:36 -0700

    Update BinaryNode.h

--- movieInventory/BinaryNode.h
+++ movieInventory/BinaryNode.h
@@ -3,10 +3,13 @@
 #include "Movie.h"
 class BinaryNode {
 public:
+	/** defalt constructor   **/
 	BinaryNode();
+	/** constructor which take movie, node pointer left and right as parametr   **/
 	BinaryNode(Movie *m, BinaryNode *left, BinaryNode *right);
+	 /** destructor   **/
 	virtual ~BinaryNode();
-
+         /** cheek if its leaf or not   **/
 	bool isLeaf();
 
 	Movie* data;
