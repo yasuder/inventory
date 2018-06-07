7751c61f603c382532146ae6da7a9f556e92e899
GOITOM K HADISHE <goitomhadishe@yahoo.com>
2018-06-06 23:14:02 -0700

    Update Inventory.h

--- movieInventory/Inventory.h
+++ movieInventory/Inventory.h
@@ -21,23 +21,37 @@ using namespace std;
 
 class Inventory {
 public:
+	
+	 /** defalt constructor  **/
 	Inventory();
+	/** destructor which cleen up un nessery Inventory  **/
 	virtual ~Inventory();
-
+        /** boolean method which take string as parameter and add movie, if add is succesed it will return true
+         * if add is not succsefull it will return false **/
 	bool addMovie(string str);
-
+       /** boolean  method which take id, first name and lastname as parameter
+         * and will return true if add is sucss, and it will return false if add is not sucss **/
 	bool addCustomer(string id, string lastName, string firstName);
-
+        /** borrrow method, which boorow the assocate move title to the assocate customer id
+         * if succs will return true if not will return false **/
 	bool borrowMovie(string title, string customerID);
+	 /** return movie  method, which take  movie title and customer id.
+          * if the specifec movie is return then it will return true,else will return false;
+           * t **/
 	bool returnMovie(string title, string customerID);
-
+         /** desplay all the inventory  **/
 	void printInventory();
+	/** print the history of individual customer  **/
 	void printHistory(string customerID);
+	  /** get the history of the customer  **/
 	TransactionList getHistory(string customerID);
-
+         /** read the txt file for the customer **/
 	void readCustomerFile(string filename);
+	/** read the movie txt file  **/
 	void readMovieFile(string filename);
+	/** read the command txt file  **/
 	void readCommandFile(string filename);
+	  /** read the excute txt file  **/
 	void executeCommand(string str);
 
 private:
